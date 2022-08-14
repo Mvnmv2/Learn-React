a24f2e2113ed33d14aa32f07423cd640537a31fc
Mvnmv2 <maven23@rambler.ru>
2022-08-14 12:18:21 +0300

    lesson 51 Users API

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,36 +1,15 @@
 import React from "react";
 import styles from "./users.module.css";
+import * as axios from 'axios';
+import userPhoto from '../../assets/images/userPhoto.png'
 
 let Users = (props) => {
 
     if (props.users.length === 0) {
-        props.setUsers([
-                {
-                    id: 1,
-                    photoUrl: 'https://avatars.mds.yandex.net/i?id=633087f63747e99f596695c2049ef8cc_l-5300097-images-thumbs&ref=rim&n=13&w=640&h=640',
-                    followed: true,
-                    fullName: 'Dmitry',
-                    status: 'I am are boss',
-                    location: {city: 'Minsk', country: 'Belarus'}
-                },
-                {
-                    id: 2,
-                    photoUrl: 'https://crosti.ru/patterns/00/20/57/ca4e737a8a/picture.jpg',
-                    followed: false,
-                    fullName: 'Andrey',
-                    status: 'Eat sleep code repeat',
-                    location: {city: 'Moscow', country: 'Russia'}
-                },
-                {
-                    id: 3,
-                    photoUrl: 'https://sun1.userapi.com/sun1-56/s/v1/ig2/Xvy6J25YIcjj6sz_FGm2m9wkX9JJnlYeDVkVQtZadSHsQKIqr_Tmr2xSJIRmnjZ6C4Iien0bAXUev3DtQeo1ScuG.jpg?size=200x200&quality=96&crop=264,0,719,719&ava=1',
-                    followed: true,
-                    fullName: 'Ioshito',
-                    status: 'What is going on there?',
-                    location: {city: 'Tokio', country: 'Japan'}
-                },
-            ]
-        )
+
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response => {
+            props.setUsers(response.data.items)
+        });
     }
 
 
@@ -40,7 +19,7 @@ let Users = (props) => {
             {
                 props.users.map(u => <div key={u.id}>
                     <span>
-                        <div> <img src={u.photoUrl} className={styles.usersPhoto} alt="photoUser"/></div>
+                        <div> <img src={u.photos.small != null ? u.photos.small : userPhoto } className={styles.usersPhoto} alt="photoUser"/></div>
                         <div>
                             {u.followed
                                 ? <button onClick={() => {
@@ -54,12 +33,12 @@ let Users = (props) => {
                     </span>
                     <span> 
                         <span>
-                            <div>{u.fullName}</div>
+                            <div>{u.name}</div>
                             <div>{u.status}</div>
                         </span>
                         <span>
-                            <div>{u.location.country}</div>
-                            <div>{u.location.city}</div>
+                            <div>{"u.location.country"}</div>
+                            <div>{"u.location.city"}</div>
                         </span>
                     </span>
                 </div>)
